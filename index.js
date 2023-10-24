ae7ca3bc8dee2ad4fd225732635d95dd00fc1e3d
Alex Kitsul <kitsulalex@gmail.com>
2023-10-23 18:55:18 -0700

    Draw Geometry

--- index.js
+++ index.js
@@ -19,6 +19,62 @@ async function main() {
         device: device,
         format: canvasFormat,
     });
+
+    const verticies = new Float32Array([
+        // X      Y
+        -0.8,  -0.8, // Triangle 1 (blue)
+         0.8,  -0.8,
+         0.8,   0.8,
+
+        -0.8,  -0.8, // Triangle 2 (red)
+         0.8,   0.8,
+        -0.8,   0.8,
+    ]);
+    const vertexBuffer = device.createBuffer({
+        label: "Cell verticies",
+        size: verticies.byteLength,
+        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    });
+    device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/0, verticies);
+    const vertexBufferLayout = {
+        arrayStride: 8,
+        attributes: [{
+            format: "float32x2",
+            offset: 0,
+            shaderLocation: 0,
+        }],
+    };
+    const cellShaderModule = device.createShaderModule({
+        label: "Cell shader",
+        code: `
+            @vertex
+            fn vertexMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
+                return vec4f(pos, 0, 1);
+            }
+
+            @fragment
+            fn fragmentMain() -> @location(0) vec4f {
+                return vec4f(0, 0, 0.6, 1);
+            }
+        `
+    });
+    const cellPipeline = device.createRenderPipeline({
+        label: "Cell pipeline",
+        layout: "auto",
+        vertex: {
+            module: cellShaderModule,
+            entryPoint: "vertexMain",
+            buffers: [vertexBufferLayout],
+        },
+        fragment: {
+            module: cellShaderModule,
+            entryPoint: "fragmentMain",
+            targets: [{
+                format: canvasFormat
+            }]
+        },
+    });
+
     const encoder = device.createCommandEncoder();
     const pass = encoder.beginRenderPass({
         colorAttachments: [{
@@ -28,6 +84,9 @@ async function main() {
             storeOp: "store",
         }]
     });
+    pass.setPipeline(cellPipeline);
+    pass.setVertexBuffer(0, vertexBuffer);
+    pass.draw(verticies.length / 2);
     pass.end();
     device.queue.submit([encoder.finish()]);
 }
